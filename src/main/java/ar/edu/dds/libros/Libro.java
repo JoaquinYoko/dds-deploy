b2d680714cf4c301d629b8c966d464b4cf9485a9
jnegrete <jnegrete@frba.utn.edu.ar>
2024-10-17 11:03:47 -0300

    Update Libro.java

--- src/main/java/ar/edu/dds/libros/Libro.java
+++ src/main/java/ar/edu/dds/libros/Libro.java
@@ -1,6 +1,7 @@
 package ar.edu.dds.libros;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
